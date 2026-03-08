aee3ce10057d74faacbe7fc8d1d6d19b0e47915d
MihneaDarie <dmihnead15@gmail.com>
2026-03-08 17:27:12 +0200

    fix gitignore

--- target/release/deps/crossbeam_deque-8e3aadcec5f54113.d
+++
@@ -1,8 +0,0 @@
-C:\Users\mdarie\rust\saker-rs\target\release\deps\crossbeam_deque-8e3aadcec5f54113.d: C:\Users\mdarie\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\lib.rs C:\Users\mdarie\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\deque.rs
-
-C:\Users\mdarie\rust\saker-rs\target\release\deps\libcrossbeam_deque-8e3aadcec5f54113.rlib: C:\Users\mdarie\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\lib.rs C:\Users\mdarie\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\deque.rs
-
-C:\Users\mdarie\rust\saker-rs\target\release\deps\libcrossbeam_deque-8e3aadcec5f54113.rmeta: C:\Users\mdarie\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\lib.rs C:\Users\mdarie\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\deque.rs
-
-C:\Users\mdarie\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\lib.rs:
-C:\Users\mdarie\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\crossbeam-deque-0.8.6\src\deque.rs:
